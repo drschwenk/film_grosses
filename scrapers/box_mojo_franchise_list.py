ffe92d85ffe8c196bb8839bb0902cc7aaa366954
schwenkmetis <schwgit2@gmail.com>
2015-01-22 14:28:07 -0500

    The franchise url dictionary is working

--- scrapers/box_mojo_franchise_list.py
+++ scrapers/box_mojo_franchise_list.py
@@ -1,18 +1,22 @@
 __author__ = 'schwenk'
 
 from box_mojo import *
+from collections import defaultdict
 
 
+def strip_unicode(string):
+	'''Helper function to strip unicode characters from the data strings stored in the data dictionary'''
+	return string.decode('unicode_escape').encode('ascii','ignore')
 
 def build_franchise_chart_urls():
 	url_base = "http://boxofficemojo.com/franchises/"
 	page = urllib2.urlopen(url_base)
 	soup = BeautifulSoup(page)
 	tablelist=[]
-	table = soup.find_all("b")
+	franchise_titles = soup.find_all("b")
 
-	for item in table:
-		tablelist.append(item.parent)
+	for franchise in franchise_titles:
+		tablelist.append(franchise.parent)
 	franchise_list = tablelist[3:-1]
 
 	chart_urls = []
@@ -21,8 +25,35 @@ def build_franchise_chart_urls():
 
 	return chart_urls
 
+def build_movie_url_list(franchise_list):
+	url_base = "http://boxofficemojo.com"
 
+	franchise_url_dict = defaultdict(list)
+	for franchise in franchiseslist:
 
-franchises = build_franchise_chart_urls()
+		page = urllib2.urlopen(franchise)
+		soup = BeautifulSoup(page)
+		movie_title =soup.find_all("b")
+		title_list =[]
+		for title in movie_title:
+			parent = title.parent
+			try:
+				new_url = url_base + parent['href']
+				if new_url not in title_list:
+					title_list.append(new_url)
+			except KeyError:
+				pass
 
-print franchises
+		title_list_ustrip = [strip_unicode(title) for title in title_list]
+		key = title_list_ustrip[1].split('=')[2].split('.')[0]
+		franchise_url_dict[key] = title_list_ustrip[2:]
+
+	return franchise_url_dict
+
+franchiseslist = build_franchise_chart_urls()
+movie_urls = build_movie_url_list(franchiseslist)
+
+print movie_urls
+for k,v in movie_urls.iteritems():
+	for link in v:
+		print k, link
