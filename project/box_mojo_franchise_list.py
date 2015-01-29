4f3fdffea2a32659b29baca3f7873dc9bc270f54
schwenkmetis <schwgit2@gmail.com>
2015-01-29 08:58:19 -0500

    Chnges to naming structure in the luther projects directory

---
+++ project/box_mojo_franchise_list.py
@@ -0,0 +1,64 @@
+__author__ = 'schwenk'
+
+from box_mojo import strip_unicode
+from collections import defaultdict
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import dateutil.parser
+import pylab
+
+
+# def strip_unicode(string):
+# 	'''Helper function to strip unicode characters from the data strings stored in the data dictionary'''
+# 	return string.decode('unicode_escape').encode('ascii','ignore')
+
+def build_franchise_chart_urls():
+	'''Parses the franchises page on boxofficemojo.com and returns
+	a list of the urls pointing to the individual franchise chart pages.
+	'''
+	url_base = "http://boxofficemojo.com/franchises/"
+	page = urllib2.urlopen(url_base)
+	soup = BeautifulSoup(page)
+	tablelist=[]
+	franchise_titles = soup.find_all("b")
+
+	for franchise in franchise_titles:
+		tablelist.append(franchise.parent)
+	franchise_list = tablelist[3:-1]
+
+	chart_urls = []
+	for franchise in franchise_list:
+		chart_urls.append(url_base + franchise['href'])
+
+	return chart_urls
+
+def build_movie_url_list(franchise_list):
+	'''Takes a list containing urls for a particular franchise chart page, parses it, and returns a
+	dictionary of the form {(franchise name):[movies in that franchise]},
+	where the movies franchise value is a list of urls pointing to the individual pages for that franchise
+	'''
+	url_base = "http://boxofficemojo.com"
+	adjust_gross_url = "&adjust_yr=2015&p="+"htm"
+
+	franchise_url_dict = defaultdict(list)
+	for franchise in franchise_list:
+		page = urllib2.urlopen(franchise)
+		soup = BeautifulSoup(page)
+		movie_title =soup.find_all("b")
+		title_list =[]
+		for title in movie_title:
+			parent = title.parent
+			try:
+				new_url = url_base + parent['href'] +adjust_gross_url
+				if new_url not in title_list and parent['href']:
+					title_list.append(new_url)
+			except KeyError:
+				pass
+
+		title_list_ustrip = [strip_unicode(title) for title in title_list]
+		key = title_list_ustrip[1].split('=')[2].split('.')[0]
+		franchise_url_dict[key] = title_list_ustrip[2:]
+
+	return franchise_url_dict
+
---
+++ project/pandas_modules.py
@@ -0,0 +1,93 @@
+
+__author__ = 'schwenk'
+
+import pylab
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import dateutil
+import dateutil.parser
+from collections import defaultdict
+
+'''
+['title',
+			'domestic_total_gross',
+			'release_date',
+			'runtime',
+			'rating',
+			'budget',
+			'genre',
+			'distrib',
+			'oscars'
+'''
+
+with open("complete_data.pkl", 'r') as picklefile:
+    complete_data_for_pandas = pickle.load(picklefile)
+
+
+df = pd.DataFrame(complete_data_for_pandas)
+
+
+# df['domestic_total_gross'].hist()
+# plt.title('Histogram of Movies by Box Office Revenues')
+# plt.xlabel('Box Office (Billions of Dollars)')
+# plt.ylabel('Number of Movies')
+# plt.show()
+
+# df['budget'].hist()
+# plt.title('Histogram of Movies by Box Office Revenues')
+# plt.xlabel('budget (hundreds of milions of Dollars)')
+# plt.ylabel('Number of Movies')
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# plt.scatter(df.budget, df.domestic_total_gross)
+# plt.title('How does budget relate to final box office revenue?\n')
+# plt.xlabel('Budget')
+# plt.ylabel('Box Office')
+# plt.show()
+
+
+
+
+sortedbyruntime = df.sort(columns='budget')
+# print sortedbyruntime.head()
+
+# releasedateplot = df.plot(x="release_date",y="domestic_total_gross")
+# plt.show()
+
+# runtimeplot = sortedbyruntime.scatter(x="release_date",y="domestic_total_gross")
+# plt.show()
+
+mydata = sortedbyruntime[["budget", "domestic_total_gross"]].dropna(how="any")
+# Now plot with matplotlib
+vals = mydata.values
+plt.scatter(vals[:, 0], vals[:, 1])
+plt.show()
+
+
+# summed_rating_groups = df.groupby('Rating').sum()
+# numberpercat = df.groupby('Rating').size()
+# avs = summed_rating_groups.div(numberpercat,axis=0)
+#
+# rating_groups = df.groupby('Rating')
+
+def makeplot(data):
+	# fig, ax = plt.subplots(nrows=4, ncols=1,sharex=True)
+	# plt.title('Domestic grosses', fontsize=17);
+	# plt.ylabel("Millions of $", fontsize=15, labelpad=15);
+	#
+	# loopc=0
+	# for name, group in rating_groups:
+	# 	group.plot(x="ReleaseDate",y="DomesticTotalGross", ax = ax[loopc])
+	# 	loopc+=1
+
+	# plt.xlabel("Films", fontsize=15, labelpad=5);
+	# plt.axis([0.8,3.2,0,600])
+	# plt.xticks(rotation=60)
+	# plt.tick_params(axis='x', which='major', labelsize=15)
+	# plt.setp(g1_plot, color='c', alpha=0.7, linewidth=3.0)
+	# plt.setp(g2_plot, color='m',alpha=0.7, linewidth=3.0)
+	# plt.legend([g1_plot, g2_plot],loc='lower left')
+	plt.show()
