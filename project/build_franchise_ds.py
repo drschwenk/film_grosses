62ea09e29e7440934c419836dc01a4c366f5c871
schwenkmetis <schwgit2@gmail.com>
2015-03-20 23:25:22 -0400

    Cleaned up file structure

--- project/build_franchise_ds.py
+++ project/build_franchise_ds.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
 
+
 def build_franchise_chart_urls():
 	'''Parses the franchises page on boxofficemojo.com and returns
 	a list of the urls pointing to the individual franchise chart pages.
@@ -11,7 +12,7 @@ def build_franchise_chart_urls():
 	url_base = "http://boxofficemojo.com/franchises/"
 	page = urllib.request.urlopen(url_base)
 	soup = BeautifulSoup(page)
-	chart_titles=[]
+	chart_titles = []
 	franchise_titles = soup.find_all("b")
 
 	for franchise in franchise_titles:
@@ -24,24 +25,25 @@ def build_franchise_chart_urls():
 
 	return chart_urls
 
+
 def build_movie_url_list(franchise_list):
 	'''Takes a list containing urls for a particular franchise chart page, parses it, and returns a
 	dictionary of the form {(franchise name):[movies in that franchise]},
 	where the movies franchise value is a list of urls pointing to the individual pages for that franchise
 	'''
 	url_base = "http://boxofficemojo.com"
-	adjust_gross_url = "&adjust_yr=2015&p="+"htm"
+	adjust_gross_url = "&adjust_yr=2015&p=" + "htm"
 
 	franchise_url_dict = defaultdict(list)
 	for franchise in franchise_list:
 		page = urllib.request.urlopen(franchise)
 		soup = BeautifulSoup(page)
-		movie_title =soup.find_all("b")
-		title_list =[]
+		movie_title = soup.find_all("b")
+		title_list = []
 		for title in movie_title:
 			parent = title.parent
 			try:
-				new_url = url_base+ parent['href'] +adjust_gross_url
+				new_url = url_base + parent['href'] + adjust_gross_url
 				if new_url not in title_list and parent['href']:
 					title_list.append(new_url)
 			except KeyError:
@@ -53,3 +55,44 @@ def build_movie_url_list(franchise_list):
 
 	return franchise_url_dict
 
+
+def process_franchise_data(movies_df):
+	franchises_dict_list = []
+	for franchise, movies_df in franchise_groups:
+		movies_df.drop_duplicates('title', inplace=True)
+		if franchise == 'lordoftherings':
+			movies_df = movies_df[movies_df.rating != 'PG']
+		franchise_dict = {}
+		try:
+			franchise_dict['first_title'] = movies_df['title'].iloc[0]
+			franchise_dict['new_title'] = movies_df['title'].iloc[2]
+			franchise_dict['franchise'] = franchise
+			franchise_dict['prev_gross'] = (movies_df['domestic_total_gross'].iloc[0] +
+			                                movies_df['domestic_total_gross'].iloc[1]) / float(2)
+			franchise_dict['act_gross'] = movies_df['domestic_total_gross'].iloc[2]
+			franchise_dict['prev_meta'] = (movies_df['metacritic'].iloc[0] + movies_df['metacritic'].iloc[1]) / float(2)
+			franchise_dict['act_meta'] = movies_df['metacritic'].iloc[2]
+			time_d = np.timedelta64(movies_df['release_date'].iloc[1] - movies_df['release_date'].iloc[0])
+			franchise_dict['days_elapsed'] = time_d.item().total_seconds() / 86400
+			franchise_dict['genre'] = movies_df['genre'].iloc[2]
+			franchise_dict['rating'] = movies_df['rating'].iloc[2]
+			franchise_dict['director'] = movies_df['director'].iloc[2]
+
+			if (movies_df['genre'].iloc[0] == movies_df['genre'].iloc[1] == movies_df['genre'].iloc[2]):
+				franchise_dict['same_genre'] = 0.0
+			else:
+				franchise_dict['same_genre'] = 1.0
+			if (movies_df['director'].iloc[0] == movies_df['director'].iloc[1] == movies_df['director'].iloc[2]):
+				franchise_dict['same_dir'] = 0.0
+			else:
+				franchise_dict['same_dir'] = 1.0
+			if (movies_df['rating'].iloc[0] == movies_df['rating'].iloc[1] == movies_df['rating'].iloc[2]):
+				franchise_dict['same_rating'] = 0.0
+			else:
+				franchise_dict['same_rating'] = 1.0
+
+			franchises_dict_list.append(franchise_dict)
+		except IndexError:
+			print(IndexError)
+
+# collapsed_df = pd.DataFrame(franchises_dict_list)
